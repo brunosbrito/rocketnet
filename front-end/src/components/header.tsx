a1f588a658c370da21985862fe7a9ba220b31673
brunosbrito <brunobrito728@gmail.com>
2023-03-18 10:01:08 -0300

    [feat] create component header

--- front-end/src/components/header.tsx
+++ front-end/src/components/header.tsx
@@ -1,11 +1,28 @@
+import { ArrowBendDownLeft } from '@phosphor-icons/react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import '../styles/headerStyle.css'
 
 function Header() {
+  const navigate = useNavigate()
+  const location = useLocation()
+  const url = location.pathname;
+
+  console.log(url)
+
+  const verifyHome = () => {
+    if(url === '/') return 'orange'
+  }
+
+  const verifyPlans = () => {
+    if(url === '/plans') return 'orange'
+  }
+
   return(
     <div className="header">
       <ul>
-        <li>Cobertura</li>
-        <li>Planos</li>
+        <button type="button" onClick={() => navigate(-1)}><ArrowBendDownLeft size={20}  /></button>
+        <li style={{color: verifyHome()}}>Cobertura</li>
+        <li style={{color: verifyPlans()}}>Planos</li>
         <li>Carrinho</li>
       </ul>
     </div>
